889471547c6baa087d1db476d65860188f877604
Igor Popov <ihptru@gmail.com>
2011-11-03 19:27:23 +0300

    multilanguage support

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "
+<div class=\"lang\">
+	<a href=\"index.php?lang=en\">English</a>
+	<a href=\"index.php?lang=ru\">Русский</a>
+	<a href=\"index.php?lang=de\">Deutsch</a>
+</div>
+";
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<head></head><title>OpenRA Content Website</title>";
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\" /></head>";
+
+
+include_once("config.php");
+
+echo $lang['test'];
+
+include("footer.php");
+?>
