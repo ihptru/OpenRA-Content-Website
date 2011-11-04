03c22cdc936e4b4efab6d75f1c15c4650f8bf1b3
Igor Popov <ihptru@gmail.com>
2011-11-04 21:03:33 +0300

    fix db_pgsql.php

--- db_pgsql.php
+++ db_pgsql.php
@@ -23,12 +23,23 @@
             return (db::$con != null);
         }
         
+        public static function nextRowFromQuery($result)
+        {
+            return pg_fetch_assoc($result);
+        }
+        
+        public static function getTableNameFrom($result, $row)
+        {
+            return pg_field_table($result, $row);
+        }
+        
         public static function setup()
         {
             $query = "CREATE TABLE users (uid serial PRIMARY KEY NOT NULL,
                                           pass VARCHAR NOT NULL,
                                           nick VARCHAR NOT NULL,
                                           email VARCHAR NOT NULL,
+                                          avatar VARCHAR NOT NULL,
                                           register_date TIMESTAMP NOT NULL);";
             db::executeQuery($query);
             
@@ -46,9 +57,11 @@
                                          posted TIMESTAMP NOT NULL);";
             db::executeQuery($query);
             
-            $query = "CREATE TABLE news (uid serial PRIMARY KEY NOT NULL,
+            //Used on front page
+            $query = "CREATE TABLE articles (uid serial PRIMARY KEY NOT NULL,
                                          title VARCHAR NOT NULL,
                                          content VARCHAR NOT NULL,
+                                         image VARCHAR NOT NULL,
                                          user_id VARCHAR NOT NULL,
                                          posted TIMESTAMP NOT NULL);";
             db::executeQuery($query);
@@ -56,15 +69,34 @@
             $query = "CREATE TABLE units (uid serial PRIMARY KEY NOT NULL,
                                           title VARCHAR NOT NULL,
                                           description VARCHAR NOT NULL,
+                                          preview_image VARCHAR NOT NULL,
                                           user_id VARCHAR NOT NULL,
                                           posted TIMESTAMP NOT NULL);";
             db::executeQuery($query);
             
-            $query = "CREATE TABLE guide (uid serial PRIMARY KEY NOT NULL,
+            //guide_type (modding, mapping, pixel art, utilities,..) each should have different images
+            $query = "CREATE TABLE guides (uid serial PRIMARY KEY NOT NULL,
                                           title VARCHAR NOT NULL,
                                           html_content VARCHAR NOT NULL,
+                                          guide_type VARCHAR NOT NULL,
                                           user_id VARCHAR NOT NULL,
                                           posted TIMESTAMP NOT NULL);";
+            db::executeQuery($query);
+            
+            //Special table for just featured 
+            $query = "CREATE TABLE featured (uid serial PRIMARY KEY NOT NULL,
+                                             table VARCHAR NOT NULL,
+                                             id INTEGER NOT NULL,
+                                             posted TIMESTAMP NOT NULL);";
+            db::executeQuery($query);
+            
+            //Comments made by users on articles
+            $query = "CREATE TABLE comments (uid serial PRIMARY KEY NOT NULL,
+                                             title VARCHAR NOT NULL,
+                                             content VARCHAR NOT NULL,
+                                             user_id INTEGER NOT NULL,
+                                             article_id INTEGER NOT NULL,
+                                             posted TIMESTAMP NOT NULL);";
             db::executeQuery($query);
         }
         
@@ -92,7 +124,7 @@
 
         public static function check()
         {
-            $allSystemsGo = true;
+			$allSystemsGo = true;
             if(!db::table_exists("users"))
                 $allSystemsGo = false;
             if(!db::table_exists("maps"))
@@ -101,10 +133,14 @@
                 $allSystemsGo = false;
             if(!db::table_exists("units"))
                 $allSystemsGo = false;
-            if(!db::table_exists("guide"))
+            if(!db::table_exists("guides"))
+                $allSystemsGo = false;
+            if(!db::table_exists("featured"))
+                $allSystemsGo = false;
+            if(!db::table_exists("comments"))
                 $allSystemsGo = false;
             return $allSystemsGo;
-        }
+		}
         
         public static function disconnect()
         {
