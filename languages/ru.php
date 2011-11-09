9e8f4f71997bac1fc67cc7c7ea9f154ca205c4b6
Igor Popov <ihptru@gmail.com>
2011-11-09 22:51:44 +0300

    add russian translation

--- languages/ru.php
+++ languages/ru.php
@@ -3,7 +3,27 @@
 class lang
 {
 	public static $lang = array(
-		'test' => "сообщение",
+			'website_name' => "OpenRA - Website",
+			'website_slowgun' => "Твой контент",
+			'login' => "Логин",
+			'password' => "Пароль",
+			'sign in' => "вход",
+			'activation error' => "Неверная ссылка активации",
+			'activated' => "Аккаунт активирован!",
+			'email in use' => "Кто-то уже использует этот e-mail адрес",
+			'empty fields' => "Обнажурены пустые поля, попробуйте снова",
+			'register complete' => "Регистрация завершена",
+			'activate' => "Активация",
+			'ask to activate' => "Пожалуйста, активируйте Ваш аккаунт",
+			'user exists' => "Аккаунт с таким имененм пользователя уже существует",
+			'password not match' => "Пароли не совпадают",
+			'email error' => "Неверный формат e-mail адреса",
+            'copyright' => "No rights reserved. Создатели: Holloweye и ihptru.",
+            'already requested' => "Уже была запрошена регистрация с идентичными данным",
+            'recover nouser' => "Пользователь с указанными данными не найден",
+            'nothing to activate' => "Нечего активировать",
+            'password updated' => "Пароль обновлен успешно",
+            'recover requested' => "Вы уже запросили обновление пароля",
 	);
 }
 
