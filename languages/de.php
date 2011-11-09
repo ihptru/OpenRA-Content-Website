68750a04863cb96dc9073b74f06652692ac0e678
Hans Koch <ore.sama.hans@googlemail.com>
2011-11-09 19:11:37 +0100

    Update languages/de.php

--- languages/de.php
+++ languages/de.php
@@ -1,7 +1,26 @@
 <?php
 
 $lang = array(
-        'test' => "nachricht",
+	'website_name' => "OpenRA - Webseite",
+	'website_slowgun' => "Brings your content",
+	'login' => "Login",
+	'password' => "Passwort",
+	'sign in' => "Anmelden",
+	'activation error' => "Der Aktivierungslink ist inkorrekt",
+	'activated' => "Der Account wurde Aktiviert!",
+	'email in use' => "Die Emailadresse ist berreits in verwenung",
+	'empty fields' => "Leere Felder wurden gefunden, bitte erneut eingeben",
+	'register complete' => "Registrierung ist komplett",
+	'activate' => "Aktiviert",
+	'ask to activate' => "Bitte Aktiviere zuerst dein Account",
+	'user exists' => "Benutzername ist berreits in verwendung",
+	'password not match' => "Die Passworter stimmen nicht überein",
+	'email error' => "Die Emailadresse ist inkorrekt",
+	'copyright' => "Keine Rechte vorbehalten. Die Seite wurde erstellt von Holloweye, Hammster and ihptru.",
+	'already requested' => "Eine Anfrage mit diesen Registrierungsdaten wurde bereits gesendet",
+	'recover nouser' => "Der Benutzer wurde nicht gefunden",
+	'nothing to activate' => "Nichts mehr zum Aktivieren da",
+	'password updated' => "Passwort wurde Aktualisiert",
 );
 
 ?>
